48abf0c89737a7ab974c9db4a309d3ff90e9c573
Paweł T. Jochym <jochym@users.noreply.github.com>
2021-02-05 09:49:11 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 cat > customize-local.py <<EOF
-mpi_libraries += [" -lmpi "]
+mpi_libraries += ["-lmpi"]
+remove_compile_args += ['-march=native']
 EOF
+
+echo "============= CUSTOMIZE ==============="
+cat customize.py
+
+echo "============= CUSTOMIZE-LOCAL ==============="
+cat customize-local.py
+
 
 $PYTHON -m pip install . -vv
